1c0da695d6c13fb7af539a0a88e8e7862e0ed896
ben lamothe <bl@732.one>
2019-04-18 04:12:45 +0200

    add batch transaction tests

---
+++ test/transactions.js
@@ -0,0 +1,53 @@
+const assert = require("assert");
+const tupelo = require("../lib/tupelo");
+const TUPELO_HOST = process.env.TUPELO_RPC_HOST || 'localhost:50051';
+
+const createWalletWithChain = async () => {
+  const wallet = tupelo.connect(TUPELO_HOST, {
+    walletName: "playtransactions",
+    passPhrase: "test",
+  });
+  await wallet.register();
+  let resp = await wallet.generateKey();
+  const walletKey = resp.keyAddr;
+  assert.equal(42, walletKey.length);
+  let {chainId,} = await wallet.createChainTree(walletKey);
+  return {wallet: wallet, walletKey: walletKey, chainId: chainId};
+};
+
+describe("playing transactions", function() {
+  this.timeout(30000);
+  let resp;
+
+  it("signs a batch of transactions", async ()=> {
+    let {wallet, walletKey, chainId} = await createWalletWithChain();
+
+    let path1 = "some/data";
+    let val1 = "foo";
+    let setData1 = tupelo.setDataTransaction(path1, val1);
+
+    let path2 = "other/data";
+    let val2 = "bar";
+    let setData2 = tupelo.setDataTransaction(path2, val2);
+
+    let token = "TestToken";
+    let max = 30;
+    let establishToken = tupelo.establishTokenTransaction(token, max);
+
+    let mintAmount = 15;
+    let mintToken = tupelo.mintTokenTransaction(token, mintAmount);
+
+    let txns = [setData1, setData2, establishToken, mintToken];
+
+    resp = await wallet.playTransactions(chainId, walletKey, txns);
+    assert.notEqual(resp.tip, null);
+
+    resp = await wallet.resolveData(chainId, path1);
+    assert.strictEqual(resp.data[0], val1);
+
+    resp = await wallet.resolveData(chainId, path2);
+    assert.strictEqual(resp.data[0], val2);
+
+    // TODO: add tests to check token balances when tokens stabilize
+  });
+});
