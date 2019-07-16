159c38c896c45c53422a543c1cdf23aa14292d72
Topper Bowers <topper@toppingdesign.com>
2019-07-16 16:30:17 +0200

    working put in JS and GET in node

--- v2/src/tupelo.spec.ts
+++ v2/src/tupelo.spec.ts
@@ -5,72 +5,122 @@ import './extendedglobal';
 import { p2p } from './node';
 
 import { TupeloWasm, Tupelo } from './tupelo';
-import { Transaction,SetDataPayload } from '/Users/tobowers/code/messages/build/js/transactions/transactions_pb'
+import { Transaction, SetDataPayload } from '/Users/tobowers/code/messages/build/js/transactions/transactions_pb'
 const dagCBOR = require('ipld-dag-cbor');
+
+
+// import * as Ipld from 'ipld'
+// import * as IpfsRepo from 'ipfs-repo'
+// import * as IpfsBlockService from 'ipfs-block-service'
+// import { MemoryDatastore } from 'interface-datastore'
+// import * as multihashing from 'multihashing-async'
+// import * as Block from 'ipfs-block'
+
+import CID from 'cids';
+
+const IpfsRepo:any = require('ipfs-repo');
+const IpfsBlockService:any = require('ipfs-block-service');
+const MemoryDatastore:any = require('interface-datastore').MemoryDatastore;
+const Block:any = require('ipfs-block');
+const multihashing:any = require('multihashing-async');
+
+const testIpld = async () => {
+    console.log('creating repo')
+    const repo = new IpfsRepo('test', {
+      lock: 'memory',
+      storageBackends: {
+        root: MemoryDatastore,
+        blocks: MemoryDatastore,
+        keys: MemoryDatastore,
+        datastore: MemoryDatastore
+      }
+    })
+    console.log('repo init')
+    await repo.init({})
+    await repo.open()
+    return new IpfsBlockService(repo)
+}
 
 describe('Tupelo', () => {
 
+  it('works as a datastore', async () => {
+    console.log('starting test')
+    const bs = await testIpld()
+    console.log('created bs')
+    // create a block
+    const data = Buffer.from('hello world')
+    const serialized = dagCBOR.util.serialize(data)
 
-  it('generate keys', async () => {
-    const key = await Tupelo.generateKey()
-    expect(key).to.have.length(32)
+    const multihash = await multihashing(serialized, 'sha2-256')
+
+    const cid = new CID(1, 'dag-cbor', multihash, 'base32')
+    const block = new Block(serialized, cid)
+    await bs.put(block)
+    const res = await Tupelo.teststore(bs, cid.toString())
+    expect(res).to.equal(cid.toString())
   })
-
-  it('runs end-to-end', async () => {
-    let resolve:Function,reject:Function
-    const p = new Promise((res,rej)=> { resolve = res, reject = rej})
-    var node = await p2p.createNode();
-    expect(node).to.exist;
-  
-    // const tw = await TupeloWasm.get()
-    const key = await Tupelo.generateKey()
-    const trans = new Transaction()
-  
-    const payload = new SetDataPayload()
-    payload.setPath("/hi")
-  
-    const serialized = dagCBOR.util.serialize("hihi")
-  
-    payload.setValue(new Uint8Array(serialized))
-    trans.setType(Transaction.Type.SETDATA)
-    trans.setSetDataPayload(payload)
-  
-    node.on('connection:start', (peer:any) => {
-      console.log("connecting to ", peer.id._idB58String, " started")
-    })
-  
-    node.on('error', (err:any) => {
-      console.log('error')
-    })
-
-    node.once('enoughdiscovery', async ()=> {
-      Tupelo.playTransactions(node.pubsub, key, [trans]).then(
-        (success)=> {
-          expect(success).to.be.an.instanceOf(Uint8Array)
-          resolve(true)
-        },
-        (err)=> {
-          expect(err).to.be.null
-          resolve(true)
-      })
-    })
-  
-    let connected = 0;
-
-    node.on('peer:connect', async ()=> {
-      console.log("peer connect")
-      connected++
-      if (connected >= 2) {
-        node.emit('enoughdiscovery')
-      }
-    })
-  
-    node.start(()=>{
-      console.log("node started");
-    });
-    return p
-  }).timeout(10000)
-
-
 })
+
+  // it('generate keys', async () => {
+  //   const key = await Tupelo.generateKey()
+  //   expect(key).to.have.length(32)
+  // })
+
+  // it('runs end-to-end', async () => {
+  //   let resolve:Function,reject:Function
+  //   const p = new Promise((res,rej)=> { resolve = res, reject = rej})
+  //   var node = await p2p.createNode();
+  //   expect(node).to.exist;
+
+  //   // const tw = await TupeloWasm.get()
+  //   const key = await Tupelo.generateKey()
+  //   const trans = new Transaction()
+
+  //   const payload = new SetDataPayload()
+  //   payload.setPath("/hi")
+
+  //   const serialized = dagCBOR.util.serialize("hihi")
+
+  //   payload.setValue(new Uint8Array(serialized))
+  //   trans.setType(Transaction.Type.SETDATA)
+  //   trans.setSetDataPayload(payload)
+
+  //   node.on('connection:start', (peer:any) => {
+  //     console.log("connecting to ", peer.id._idB58String, " started")
+  //   })
+
+  //   node.on('error', (err:any) => {
+  //     console.log('error')
+  //   })
+
+  //   node.once('enoughdiscovery', async ()=> {
+  //     Tupelo.playTransactions(node.pubsub, key, [trans]).then(
+  //       (success)=> {
+  //         expect(success).to.be.an.instanceOf(Uint8Array)
+  //         resolve(true)
+  //       },
+  //       (err)=> {
+  //         expect(err).to.be.null
+  //         resolve(true)
+  //     })
+  //   })
+
+  //   let connected = 0;
+
+  //   node.on('peer:connect', async ()=> {
+  //     console.log("peer connect")
+  //     connected++
+  //     if (connected >= 2) {
+  //       node.emit('enoughdiscovery')
+  //     }
+  //   })
+
+  //   node.start(()=>{
+  //     console.log("node started");
+  //   });
+  //   return p
+  // }).timeout(10000)
+
+
+// })
 
--- v2/src/tupelo.ts
+++ v2/src/tupelo.ts
@@ -1,18 +1,34 @@
 declare const Go: any;
 
+import CID from 'cids';
+
 import * as go from "./js/go"
 import { Transaction } from 'tupelo-messages'
 
 class FakePublisher {
-    public publish(topic:string, data:Uint8Array, cb:Function) {
+    public publish(topic: string, data: Uint8Array, cb: Function) {
         console.log("publishing ", data, " on ", topic, "cb ", cb)
         cb(null)
     }
 }
 
+interface IBlock {
+    data: Buffer
+}
+
+interface IDagStore {
+    get(cid: CID): Promise<Object>
+    put(block: IBlock): Promise<CID>
+    resolve(cid: CID, path: string): Iterable<Promise<{ remainderPath: string, value: any }>>
+}
+
+// interface IDagStoreWithBlockStore extends IDagStore {
+//     bs: any
+// }
+
 export interface IPubSub {
-    publish(topic:string, data:Uint8Array, cb:Function):null
-    subscribe(topic:string, onMsg:Function, cb:Function):null
+    publish(topic: string, data: Uint8Array, cb: Function): null
+    subscribe(topic: string, onMsg: Function, cb: Function): null
 }
 
 class UnderlyingWasm {
@@ -21,25 +37,28 @@ class UnderlyingWasm {
     constructor() {
         this._populated = false;
     }
-    testpubsub(publisher:IPubSub):Promise<String>{
-        return new Promise<String>((res,rej)=> {}); // replaced by wasm
+    testpubsub(publisher: IPubSub): Promise<String> {
+        return new Promise<String>((res, rej) => { }); // replaced by wasm
     }
-    generateKey():Promise<Uint8Array> {
-        return new Promise<Uint8Array>((res,rej)=> {}) // replaced by wasm
+    generateKey(): Promise<Uint8Array> {
+        return new Promise<Uint8Array>((res, rej) => { }) // replaced by wasm
     }
-    testclient(publisher:IPubSub, keys:Uint8Array, transactions:Uint8Array[]):Promise<Uint8Array> {
-        return new Promise<Uint8Array>((res,rej)=> {}) // replaced by wasm
+    testclient(publisher: IPubSub, keys: Uint8Array, transactions: Uint8Array[]): Promise<Uint8Array> {
+        return new Promise<Uint8Array>((res, rej) => { }) // replaced by wasm
+    }
+    teststore(store: IDagStore, cid: String): Promise<String> {
+        return new Promise<String>((res,rej) => {}) // replaced by wasm
     }
 }
 
 export namespace TupeloWasm {
     const _tupelowasm = new UnderlyingWasm();
-    
+
     export async function get() {
         if (_tupelowasm._populated) {
             return _tupelowasm;
         }
-    
+
         go.run("./main.wasm");
         await go.ready();
         go.populate(_tupelowasm);
@@ -50,22 +69,27 @@ export namespace TupeloWasm {
 
 export namespace Tupelo {
 
-   export async function generateKey():Promise<Uint8Array> {
-       const tw = await TupeloWasm.get()
-       return tw.generateKey()
-   }
+    export async function generateKey(): Promise<Uint8Array> {
+        const tw = await TupeloWasm.get()
+        return tw.generateKey()
+    }
 
-   export async function playTransactions(publisher:IPubSub, key:Uint8Array, transactions:Transaction[]):Promise<Uint8Array> {
-       const tw = await TupeloWasm.get()
-       console.log("serializing the bits")
-       let bits:Uint8Array[] = new Array<Uint8Array>()
-       for (var t of transactions) {
-          const serialized = t.serializeBinary()
-          bits = bits.concat(serialized)
-       }
-       console.log("testclient called")
-       return tw.testclient(publisher, key, bits)
-   }
+    export async function playTransactions(publisher: IPubSub, key: Uint8Array, transactions: Transaction[]): Promise<Uint8Array> {
+        const tw = await TupeloWasm.get()
+        console.log("serializing the bits")
+        let bits: Uint8Array[] = new Array<Uint8Array>()
+        for (var t of transactions) {
+            const serialized = t.serializeBinary()
+            bits = bits.concat(serialized)
+        }
+        console.log("testclient called")
+        return tw.testclient(publisher, key, bits)
+    }
+
+    export async function teststore(store: IDagStore, cid: String): Promise<String> {
+        const tw = await TupeloWasm.get()
+        return tw.teststore(store, cid)
+    }
 }
 
 export default Tupelo
