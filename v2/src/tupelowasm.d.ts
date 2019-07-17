02a6ebe79ce806eb252f90ddf2351565e17c6c00
Topper Bowers <topper@toppingdesign.com>
2019-07-17 14:32:37 +0200

    get rid of unnecessary .d.ts file

--- v2/src/tupelowasm.d.ts
+++
@@ -1,7 +0,0 @@
-interface JSPublisher {
-    publish(topic:string, data:Uint8Array, cb:Function):null
-}
-
-declare module '*tupelowasm' {
-    export function publish(publisher:JSPublisher):null
-}
